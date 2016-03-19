dea48c5724592b7c3026fe8a2fff971fb6d6498c
Leuli <leupold.a@googlemail.com>
2016-03-19 13:19:08 +0100

    copyjob fertig

--- copyjob.py
+++ copyjob.py
@@ -1,56 +1,51 @@
 import os
 import shutil
 
+
 class Copyjob:
 
     def __init__(self, src, dest):
         self.src = src
         self.dest = dest
         self.copied_bytes = 0
-        self.total_size = 0
+        self.total_bytes = 0
 
-    # create directory if not existing
-    def make_dir(self,destination):
-        if not os.path.exists(destination):
-            os.makedirs(destination)
+    def get_progress(self):
+        return self.copied_bytes / self.total_bytes
 
     # get the filesize in bytes for all files to be copied
-    def get_overall_filesize(self):
+    def __get_overall_filesize(self):
 
         if os.path.isdir(self.src):
             for dirpath, dirnames, filenames in os.walk(self.src):
                 for f in filenames:
                     fp = os.path.join(dirpath, f)
-                    self.total_size += os.path.getsize(fp)
+                    self.total_bytes += os.path.getsize(fp)
         else:
             print("Copyjob: Error in count_all_files_size()")
 
-        print("Copyjob: Total filesize: {}".format(self.total_size))
+        print("Copyjob: Total filesize: {}".format(self.total_bytes))
 
     # copy files and count copied bytes to measure progress
-    def copy_files(self):
+    def __copy_files(self):
 
         # traverse directory
         for dirpath, dirnames, filenames in os.walk(self.src):
-
-            # create necessary directories in dest folder
-            for directory in dirnames:
-                dest_dir = dirpath.replace(self.src, self.dest)
-                self.make_dir(os.path.join(dest_dir, directory))
 
             # copy files and add size of copied files
             for sfile in filenames:
                 src_file = os.path.join(dirpath, sfile)
                 dest_file = os.path.join(dirpath.replace(self.src, self.dest), sfile)
-                print(src_file)
-                print(dest_file)
+
+                # create destination directory
+                if not os.path.exists(os.path.dirname(dest_file)):
+                    os.makedirs(os.path.dirname(dest_file))
+
                 shutil.copy(src_file, dest_file)
                 self.copied_bytes += os.path.getsize(src_file)
-                print(self.copied_bytes)
-
-
+                print("progress: {}".format(self.get_progress()))
 
     # start the copy job
     def start_job(self):
-        self.get_overall_filesize()
-        self.copy_files()
+        self.__get_overall_filesize()
+        self.__copy_files()
