9d619ed071e3babe70ab6c19e189865377735b5f
Leuli <leupold.a@googlemail.com>
2016-03-19 22:33:40 +0100

    dropbox_uploader

--- dropbox_uploader.py
+++ dropbox_uploader.py
@@ -6,6 +6,11 @@ def tues():
     with open("/home/pi/dbx_token.txt") as f:
         dbx_token = f.read()
 
+    # connect dropbox account
     dbx = dropbox.Dropbox(dbx_token)
+    dbx.users_get_current_account()
 
-    print(dbx.users_get_current_account())
+    for entry in dbx.files_list_folder('').entries:
+        print(entry.name)
+
+def upload(localfile, backuppath):
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from time import sleep
 from copyjob import Copyjob
 import shutil
+import dropbox_uploader
 
-shutil.rmtree("/home/pi/sd_test/2")
-job1 = Copyjob("/home/pi/sd_test/1","/home/pi/sd_test/2")
-job1.start()
-while job1.is_alive():
-    sleep(1)
-    print("progress:{}   running:{}".format(job1.get_progress(), job1.is_alive()))
+dropbox_uploader.tues()
 
+
+# shutil.rmtree("/home/pi/sd_test/2")
+# job1 = Copyjob("/home/pi/sd_test/1","/home/pi/sd_test/2")
+# # job1.start()
+# while job1.is_alive():
+#     sleep(1)
+#     print("progress:{}   running:{}".format(job1.get_progress(), job1.is_alive()))
+#
